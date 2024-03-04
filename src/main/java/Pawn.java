6dfe7fb4c00f9a1c4c9138608ab595fe463d9256
ehpark <silbroeh@gmail.com>
2024-03-05 00:13:32 +0900

    feat : Pawn 클래스 구현
    생성자 및 색상 필드의 getter 구현

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,12 @@
+public class Pawn {
+
+    private final String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
