ae0126f1a3045add559faa473f1ade9bb4c65ac6
jangsubin <eltm521@naver.com>
2025-03-04 14:27:50 +0900

    test : 검은색 폰 생성 테스트 작성 - black으로 생성한 폰의 색깔이 black이면 성공

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 public class PawnTest {
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색, 검은색 폰이 생성되어야 한다")
     public void create() {
         Pawn pawn = new Pawn("white");
+        Pawn pawn2 = new Pawn("black");
         assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawn2.getColor()).isEqualTo("black");
     }
 }
