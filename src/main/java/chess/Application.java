d252ada513bfacf077e6dcff4f966ac72d651a65
jangsubin <eltm521@naver.com>
2025-03-05 11:49:58 +0900

    feat : main 메소드 생성 - 보드를 초기화 하고 출력하는 메인 메소드 생성

---
+++ src/main/java/chess/Application.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class Application {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.initialize();
+        board.print();
+    }
+}
