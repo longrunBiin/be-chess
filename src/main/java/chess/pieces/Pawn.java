4ca8df411e88c030f7b897e429a93282e739c5a4
jangsubin <eltm521@naver.com>
2025-03-05 10:58:17 +0900

    fix : 매개변수 1개 생성자 추가 - 매개변수 1개를 2개로 수정하니 BoardTest에서 에러발생 - 매개변수 1개의 생성자를 추가해서 테스트를 고치지 않도록 수정

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -6,13 +6,15 @@ public class Pawn {
     public static final char WHITE_REPRESENTATION = 'p';
     public static final char BLACK_REPRESENTATION = 'P';
     private final String color;
-    private final char representation;
+    private char representation;
 
     public Pawn(String color, char representation) {
         this.color = color;
         this.representation = representation;
     }
-
+    public Pawn(String color) {
+        this.color = color;
+    }
     public Pawn() {
         color = WHITE_COLOR;
         representation = 'p';
