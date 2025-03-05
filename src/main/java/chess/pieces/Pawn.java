d392f522a799bae167ef5dad6b7ffdc81cb00600
jangsubin <eltm521@naver.com>
2025-03-05 10:54:59 +0900

    feat : Pawn 출력문자 추가 - 생성자 두번째 매개변수로 출력문자 추가 - getRepresentation() 추가

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -3,17 +3,27 @@ package chess.pieces;
 public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final char WHITE_REPRESENTATION = 'p';
+    public static final char BLACK_REPRESENTATION = 'P';
     private final String color;
+    private final char representation;
 
-    public Pawn(String color) {
+    public Pawn(String color, char representation) {
         this.color = color;
+        this.representation = representation;
     }
 
     public Pawn() {
         color = WHITE_COLOR;
+        representation = 'p';
     }
+
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
     }
 }
