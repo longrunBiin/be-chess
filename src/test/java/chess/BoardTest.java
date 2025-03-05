c8d84f4bca2779383b17ed6399dca7c40d773598
jangsubin <eltm521@naver.com>
2025-03-05 10:26:13 +0900

    refactor : step2-2 코드 리뷰 반영 - 테스트 클래스 public 접근 지정자 제거 - AssertEqual -> AssertThat 변경

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,24 +1,24 @@
 package chess;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.pieces.Pawn;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BoardTest {
+class BoardTest {
     private Board board;
     private String[] colors;
 
     @BeforeEach
-    public void init() {
+    void init() {
         board = new Board();
         colors = new String[]{Pawn.WHITE_COLOR, Pawn.BLACK_COLOR};
     }
     @Test
     @DisplayName("폰이 추가될 Pawn의 수와 마지막에 추가된 폰이 맞는지 확인")
-    public void create() throws Exception {
+    void create() throws Exception {
         for (int i = 0; i < colors.length; i++) {
             verifyBoard(i);
         }
@@ -30,7 +30,7 @@ public class BoardTest {
         Pawn pawn = new Pawn(colors[i]);
         board.add(pawn);
 
-        assertEquals(i +1, board.size());
-        assertEquals(pawn, board.findPawn(i));
+        assertThat(board.size()).isEqualTo(i+1);
+        assertThat(board.findPawn(i)).isEqualTo(pawn);
     }
 }
