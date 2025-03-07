424adfcced8e9d80b6af5f2bd828c19e299a1327
jangsubin <eltm521@naver.com>
2025-03-07 12:20:41 +0900

    refactor : main() 분리 클래스 생성 및 메소드 명 변경

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,11 +1,16 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Board board = new Board();
+        List<Rank> chessBoard = new ArrayList<>();
+        Board board = new Board(chessBoard);
+        ChessGame chessGame = new ChessGame(chessBoard);
+        ChessView chessView = new ChessView(chessBoard);
 
         System.out.println("게임을 시작하려면 start, 끝내려면 end를 입력하세요");
         while (true) {
@@ -13,11 +18,11 @@ public class Application {
             if (input.equals("end")) return;
             else if(input.equals("start")){
                 board.initialize();
-                board.print();
+                chessView.print();
             } else if (input.startsWith("move")) {
                 String[] splitInput = input.split(" ");
-                board.move(splitInput[1], splitInput[2]);
-                board.print();
+                chessGame.move(splitInput[1], splitInput[2]);
+                chessView.print();
             }
             System.out.println("기물을 움직이려면 move start end 를 입력하세요");
         }
