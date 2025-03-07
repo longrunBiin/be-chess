5b5b18f2a1b59ea99f2a9e0e2030633bb7262916
jangsubin <eltm521@naver.com>
2025-03-07 12:04:34 +0900

    feat : 사용자 입력으로 이동 구현 - 사용자가 move를 입력한 경우 기물 이동

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -6,13 +6,20 @@ public class Application {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Board board = new Board();
-        board.initialize();
 
+        System.out.println("게임을 시작하려면 start, 끝내려면 end를 입력하세요");
         while (true) {
             String input = in.nextLine();
             if (input.equals("end")) return;
-            if (input.equals("start"))
+            else if(input.equals("start")){
+                board.initialize();
                 board.print();
+            } else if (input.startsWith("move")) {
+                String[] splitInput = input.split(" ");
+                board.move(splitInput[1], splitInput[2]);
+                board.print();
+            }
+            System.out.println("기물을 움직이려면 move start end 를 입력하세요");
         }
     }
 }
