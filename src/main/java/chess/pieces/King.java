2a46be139430d56cc0f300d97ab1d7da61bd7987
jangsubin <eltm521@naver.com>
2025-03-09 14:18:33 +0900

    refactor : King 리팩토링 - for문이 아닌 스트림으로 변경

--- src/main/java/chess/pieces/King.java
+++ src/main/java/chess/pieces/King.java
@@ -10,10 +10,20 @@ public class King extends Piece{
 
     @Override
     public void verifyMovePosition(int startX, int startY, int endX, int endY) {
-        for (Direction direction : directionList) {
-            if (endX - startX == direction.getXDegree() && endY - startY == direction.getYDegree())
-                return;
+        int dx = endX - startX;
+        int dy = endY - startY;
+
+        Direction moveDirection = findDirection(dx, dy);
+        if (moveDirection == null || !directionList.contains(moveDirection)) {
+            throw new IllegalArgumentException("킹은 자신을 중심으로 8방향으로 한칸씩 이동할 수 있습니다.");
         }
-        throw new IllegalArgumentException("킹은 자신을 중심으로 8방향으로 한칸만 움직일 수 있습니다.");
+    }
+    private Direction findDirection(int dx, int dy) {
+        if (dx == 0 && dy == 0) return null; // 자기 위치 유지
+
+        return directionList.stream()
+                .filter(d -> (d.getXDegree() == dx && d.getYDegree() == dy))
+                .findFirst()
+                .orElse(null);
     }
 }
