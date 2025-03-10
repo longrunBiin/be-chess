968d13bd1b8db8e9669f4d5836c0b05d7a9f7ec4
jangsubin <eltm521@naver.com>
2025-03-10 16:37:52 +0900

    reafactor : 사용자 입력 받는 부분 분리

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
+
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         List<Rank> chessBoard = new ArrayList<>();
@@ -14,21 +16,29 @@ public class Application {
 
         System.out.println("게임을 시작하려면 start, 끝내려면 end를 입력하세요");
         while (true) {
-            try{
-                String input = in.nextLine();
-                if (input.equals("end")) return;
-                else if(input.equals("start")){
-                    board.initialize();
-                    chessView.print();
-                } else if (input.startsWith("move")) {
-                    String[] splitInput = input.split(" ");
-                    chessGame.move(splitInput[1], splitInput[2]);
-                    chessView.print();
-                }
-                System.out.println("기물을 움직이려면 move start end 를 입력하세요");
-            }catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage() + "다시 입력하세요.");
+            if (!readUserCommand(in, board, chessView, chessGame)) {
+                return;
             }
         }
+    }
+
+    private static boolean readUserCommand(Scanner in, Board board, ChessView chessView, ChessGame chessGame) {
+        try{
+            String input = in.nextLine();
+            if (input.equals("end"))
+                return false;
+            else if(input.equals("start")){
+                board.initialize();
+                chessView.print();
+            } else if (input.startsWith("move")) {
+                String[] splitInput = input.split(" ");
+                chessGame.move(splitInput[1], splitInput[2]);
+                chessView.print();
+            }
+            System.out.println("기물을 움직이려면 move start end 를 입력하세요");
+        }catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage() + "다시 입력하세요.");
+        }
+        return true;
     }
 }
