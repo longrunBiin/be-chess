e5492f101bc1a480fecb6131f2e4c9978bb87783
jangsubin <eltm521@naver.com>
2025-03-13 16:58:06 +0900

    fix : 폰 대각선 이동 해결 - 대각선 아래로 내려갈때 적을 못잡는 문제 해결

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -37,7 +37,7 @@ public class Pawn extends Piece{
             }
         }else {
             // 첫 번째 이후에는 1칸만 전진 가능
-            if (dy == 1) {
+            if (dy == 1 || dy == -1) {
                 return;
             }
             throw new IllegalArgumentException("폰은 한 번 이동 후 1칸만 이동할 수 있습니다.");
